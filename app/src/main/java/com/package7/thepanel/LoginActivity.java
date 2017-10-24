44d9c671cd5e43ad02e45742c201058f06caa450
george-package7 <31438346+george-package7@users.noreply.github.com>
2017-10-24 12:52:32 +0100

    Started empty LoginActivity

---
+++ app/src/main/java/com/package7/thepanel/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.package7.thepanel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import java.io.IOException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
