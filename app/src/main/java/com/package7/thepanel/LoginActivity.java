553be48cc575be921ed1bf61e1a224b1c178a133
george-package7 <31438346+george-package7@users.noreply.github.com>
2017-10-24 13:16:28 +0100

    Created test Snackbar

--- app/src/main/java/com/package7/thepanel/LoginActivity.java
+++ app/src/main/java/com/package7/thepanel/LoginActivity.java
@@ -1,17 +1,30 @@
 package com.package7.thepanel;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import java.io.IOException;
-import java.net.URL;
-import javax.net.ssl.HttpsURLConnection;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ConstraintLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        coordinatorLayout = (ConstraintLayout) findViewById(R.id.constraintLayout);
+        Snackbar snackbar = Snackbar
+                .make(coordinatorLayout, "No internet connection!", Snackbar.LENGTH_LONG)
+                .setAction("RETRY", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                    }
+                });
+
+        snackbar.setActionTextColor(Color.RED);
     }
 }
